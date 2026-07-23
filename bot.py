1131419336f5676cdde18b111427d6a9ec8956d2
sarah9898 <59569384+sarah9898@users.noreply.github.com>
2026-07-23 19:43:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 config.py
+from telegram.ext import Application, CommandHandler
+from config import BOT_TOKEN
+from handlers.start import start
+
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+
+print("Bot Kedaiii Ona aktif")
+
+app.run_polling()
