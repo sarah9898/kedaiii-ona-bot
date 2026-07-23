682ccee40d5491b23c31df30c560b5b9990e95bc
sarah9898 <59569384+sarah9898@users.noreply.github.com>
2026-07-23 19:46:35 +0700

    Update main_menu.py

--- handlers/keyboards/main_menu.py
+++ handlers/keyboards/main_menu.py
@@ -1,1 +1,21 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+def menu():
+
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "📱 Order",
+                callback_data="order"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "💳 Pembayaran",
+                callback_data="payment"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
